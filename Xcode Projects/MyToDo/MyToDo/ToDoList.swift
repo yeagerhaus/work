c72bbc82ec10df2fdef21bbc48a8cfc05a6cf620
Cole Yeager <yeagerboy117@gmail.com>
2020-07-03 19:09:31 -0500

    Updated MyToDo

--- Xcode Projects/MyToDo/MyToDo/ToDoList.swift
+++ Xcode Projects/MyToDo/MyToDo/ToDoList.swift
@@ -2,8 +2,33 @@
 //  ToDoList.swift
 //  MyToDo
 //
-//  Created by Laptop on 7/3/20.
+//  Created by Cole Yeager on 7/3/20.
 //  Copyright © 2020 Cole Yeager. All rights reserved.
 //
 
 import Foundation
+
+class ToDoItem
+{
+    var title: String
+    var done: Bool
+
+    public init(title: String)
+    {
+        self.title = title
+        self.done = false
+    }
+}
+
+extension ToDoItem
+{
+    public class func getMockData() -> [ToDoItem]
+    {
+        return [
+            ToDoItem(title: "Milk"),
+            ToDoItem(title: "Chocolate"),
+            ToDoItem(title: "Light bulb"),
+            ToDoItem(title: "Dog food")
+        ]
+    }
+}
